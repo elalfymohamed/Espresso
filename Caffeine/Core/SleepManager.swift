9165712f0241b6b5ba4f528d9ad53cc20e75b39b
elalfymohamed <elalfy.mohamed19@gmail.com>
2026-07-03 00:08:21 +0300

    Create SleepManager.swift

---
+++ Caffeine/Core/SleepManager.swift
@@ -0,0 +1,57 @@
+//
+//  SleepManager.swift
+//  Caffeine
+//
+//  Created by Alfy on 02/07/2026.
+//
+
+import IOKit.pwr_mgt
+import AppKit
+
+final class SleepManager: NSObject {
+    static let shared = SleepManager()
+    private var assertionID: IOPMAssertionID = 0
+    private var assertionActive = false
+    
+    private override init() {
+            super.init()
+            NotificationCenter.default.addObserver(
+                self,
+                selector: #selector(handleAppTermination),
+                name: NSApplication.willTerminateNotification,
+                object: nil
+            )
+        }
+    
+    @objc private func handleAppTermination() {
+            allowSleep()
+        }
+    
+    func requestSleepAssertion() {
+        guard !assertionActive else {return}
+        
+        let reason = "Caffeine app keeping system awake" as CFString
+        let success = IOPMAssertionCreateWithName(
+            kIOPMAssertionTypeNoDisplaySleep as CFString,
+            IOPMAssertionLevel(kIOPMAssertionLevelOn),
+            reason,
+            &assertionID
+        )
+        
+        if success != kIOReturnSuccess {
+            assertionActive = true
+        }
+    }
+    
+    func allowSleep(){
+        guard assertionActive else {return}
+        
+        let result = IOPMAssertionRelease(assertionID)
+        if result  == kIOReturnSuccess {
+            assertionActive = false
+        }
+        
+    }
+    
+    
+}
