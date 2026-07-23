893189ce12e5709fcf45353d95071a92195fd223
elalfymohamed <elalfy.mohamed19@gmail.com>
2026-07-23 03:09:27 +0300

    chore: update DateUtils

--- Caffeine/Utilities/DateUtils.swift
+++ Caffeine/Utilities/DateUtils.swift
@@ -19,9 +19,23 @@ struct DateUtils {
         )!
     }
     
+    static func getTimeMinutes(from date: Date) -> Int {
+        let now = Date()
+        var target = date
+        if target < now {
+            target = Calendar.current.date(byAdding: .day, value: 1, to: target) ?? target
+        }
+        let seconds = target.timeIntervalSince(now)
+        return max(0, Int(seconds) / 60)
+    }
+    
     static func getTimeString(from date: Date) -> String {
         let formatter = DateFormatter()
         formatter.timeStyle = .short
         return formatter.string(from: date)
+    }
+    
+    static func formattedTime(_ seconds: Int) -> String {
+            String(format: "%02d:%02d", seconds / 60, seconds % 60)
     }
 }
