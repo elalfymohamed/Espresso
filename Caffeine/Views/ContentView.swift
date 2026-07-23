2f51cb4fc84e381030e515b07ec6057f82ea2528
elalfymohamed <elalfy.mohamed19@gmail.com>
2026-07-23 03:09:37 +0300

    chore: update ContentView

--- Caffeine/Views/ContentView.swift
+++ Caffeine/Views/ContentView.swift
@@ -7,36 +7,48 @@
 
 import SwiftUI
 
-
-
 struct ContentView: View {
-    @State var selectedTime: String = ""
-    @Binding var isCaffeineOn : Bool
+    @State var selected: String = "Activate for"
+    @State var sleectedTime: Int = 0
+    @Binding var isCaffeineOn: Bool
+    
+    @State private var caffeineTimer = CaffeineTimer()
+    
     var body: some View {
-        VStack(){
+        VStack {
             Text("Caffeine")
-                    .foregroundColor(isCaffeineOn ? .brown : .gray)
-                    .font(.headline)
-                
-            if !selectedTime.isEmpty{
-                Button(action:{
-                    SleepManager.shared.allowSleep()
-                    selectedTime = ""
-                    isCaffeineOn = false
-                }){
-                    Text(selectedTime)
-                        .foregroundColor(.black)
-                     .font(.headline)
-                }
+                .foregroundColor(isCaffeineOn ? .brown : .gray)
+                .font(.headline)
+
+            if isCaffeineOn {
+                RemainingSeconds(isCaffeineOn: $isCaffeineOn, selected: $selected,caffeineTimer: caffeineTimer)
+             }
+            DropDownMenu(selected: $selected, sleectedTime: $sleectedTime, isCaffeineOn: $isCaffeineOn)
+        }
+        .onAppear {
+                caffeineTimer.onExpire = { isCaffeineOn = false }
             }
-            
-            DropDownMenu(selectedTime: $selectedTime, isCaffeineOn: $isCaffeineOn)
+        .onChange(of: isCaffeineOn) { _, newValue in
+            if newValue {
+                caffeineTimer.startTimer(durationMinutes: sleectedTime)
+                SleepManager.shared.preventSleep()
+            } else {
+                caffeineTimer.stopTimer()
+                SleepManager.shared.allowSleep()
+                selected = "Activate for"
+            }
+        }
+        .onChange(of: sleectedTime) { _, newDuration in
+            guard isCaffeineOn else { return }
+            caffeineTimer.startTimer(durationMinutes: newDuration)
         }
         
     }
+
+    
 }
 
 #Preview {
     @Previewable @State var isCaffeineOn: Bool = false
-    ContentView(isCaffeineOn: $isCaffeineOn)
+    ContentView(sleectedTime: 15, isCaffeineOn: $isCaffeineOn)
 }
--- Caffeine/Views/DropDownMenu.swift
+++ Caffeine/Views/DropDownMenu.swift
@@ -6,61 +6,54 @@
 //
 import SwiftUI
 
+import SwiftUI
+
 struct DropDownMenu: View {
-    @State var selected:String = "Activate for"
-    @State private var showingTime = false
-    @Binding var selectedTime: String;
-    @Binding var isCaffeineOn : Bool
-    
-    func addTime(_ minutes: Int){
-        let newTime: Date = DateUtils.getTime(minutes: minutes)
-        let formatter = DateUtils.getTimeString(from: newTime)
-        selectedTime = minutes == -1 ? "Forever" : "Stop at \(formatter)"
-    }
-    
+    @State private var showingCustomize = false
+    @Binding  var selected: String
+    @Binding var sleectedTime: Int
+    @Binding var isCaffeineOn: Bool
+
     var body: some View {
         VStack {
             Menu {
                 ForEach(TimeConstants.options) { item in
                     Button {
-                        addTime(item.value)
+                        sleectedTime = item.value
                         selected = item.label
                         isCaffeineOn = true
-                        SleepManager.shared.requestSleepAssertion()
-                    }label:{
+                    } label: {
                         HStack {
-                         Text(item.label)
+                            Text(item.label)
                             if selected == item.label {
                                 Image(systemName: "checkmark")
-                                  .foregroundColor(.blue)
-                                }
+                            }
                         }
                     }
                 }
-                .padding()
-                .frame(maxWidth: 100, maxHeight: 100)
-                Button("Customize") {
-                    showingTime = true
-                }
+                Divider()
                 
+                Button("Customize") {
+                    showingCustomize = true
+                }
             } label: {
                 Text(selected)
                     .font(.system(size: 14, weight: .light, design: .rounded))
-                
             }
             .menuStyle(.automatic)
-            .padding(10)
+            .padding(.top, 5)
+            .padding(.bottom, 8)
             .frame(maxWidth: 150, maxHeight: 60)
-            .popover(isPresented: $showingTime) {
-                CustomTimePickerView(selected: $selected)
+            .popover(isPresented: $showingCustomize) {
+                CustomTimePickerView(selected: $selected, sleectedTime: $sleectedTime)
             }
         }
     }
 }
 
 #Preview {
-    @Previewable @State var selectedTime: String = "";
-    @Previewable @State var isCaffeineOn: Bool = false;
-
-    DropDownMenu(selectedTime: $selectedTime, isCaffeineOn:$isCaffeineOn)
+    @Previewable @State var sleectedTime: Int = 0
+    @Previewable @State var isCaffeineOn: Bool = false
+    @Previewable @State var selected: String = "Activate for"
+    DropDownMenu(selected: $selected, sleectedTime: $sleectedTime, isCaffeineOn: $isCaffeineOn)
 }
