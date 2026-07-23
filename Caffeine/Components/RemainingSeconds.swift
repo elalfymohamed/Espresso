97e94eee9f6d8edcc8ffd743cd956ef6b89bc934
elalfymohamed <elalfy.mohamed19@gmail.com>
2026-07-23 03:10:39 +0300

    feat: add RemainingSeconds

---
+++ Caffeine/Components/RemainingSeconds.swift
@@ -0,0 +1,38 @@
+//
+//  RemainingSeconds.swift
+//  Caffeine
+//
+//  Created by Alfy on 23/07/2026.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct RemainingSeconds: View {
+    @Binding var isCaffeineOn: Bool
+    @Binding var selected: String
+    @ObservedObject var caffeineTimer: CaffeineTimer
+
+    var body: some View {
+        Button {
+            isCaffeineOn = false
+            selected = "Activate for"
+        } label: {
+            if caffeineTimer.remainingSeconds > 0 {
+                Text("\(DateUtils.formattedTime(caffeineTimer.remainingSeconds)) remaining")
+            } else {
+                Text("On indefinitely — tap to stop")
+            }
+        }
+        .foregroundColor(.brown)
+        .font(.headline)
+    }
+}
+
+#Preview {
+    @Previewable @State var isCaffeineOn: Bool = false
+    @Previewable @State var selected: String = "Activate for"
+    
+    RemainingSeconds(isCaffeineOn: $isCaffeineOn,selected: $selected,  caffeineTimer: CaffeineTimer())
+}
