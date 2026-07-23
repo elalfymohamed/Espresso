a5a8a6f7a5818462c425c5a0e8e8b97a42d8f893
elalfymohamed <elalfy.mohamed19@gmail.com>
2026-07-23 03:08:02 +0300

    chore: update CaffeineAPP.swift

--- Caffeine/CaffeineApp.swift
+++ Caffeine/CaffeineApp.swift
@@ -6,21 +6,28 @@
 //
 
 import SwiftUI
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationWillTerminate(_ notification: Notification) {
+        SleepManager.shared.allowSleep()
+    }
+}
 
 @main
 struct CaffeineApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     @State var isCaffeineOn: Bool = false
-    
     
     var body: some Scene {
         MenuBarExtra {
             ContentView(isCaffeineOn: $isCaffeineOn)
+                .padding(.top)
         } label: {
             Label("Caffeine", systemImage: isCaffeineOn ? "cup.and.heat.waves.fill" : "cup.and.saucer.fill")
                 .contentTransition(.symbolEffect(.replace))
                 .animation(.default, value: isCaffeineOn)
         }
-        .menuBarExtraStyle(.automatic)
-       
+        .menuBarExtraStyle(.window)
     }
 }
